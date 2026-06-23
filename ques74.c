d4466d6ffc05783e46cc1aca811700ca2032985f
brahmanluv09-sketch <brahmanluv09@gmail.com>
2026-06-23 18:34:43 +0530

    Add files via upload
    This day introduces matrix operations:
    
    Matrix addition
    Matrix subtraction
    Matrix transpose
    Diagonal sum

---
+++ ques74.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], b[r][c], sub[r][c];
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            sub[i][j] = a[i][j] - b[i][j];
+
+    printf("Subtraction Matrix:\n");
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+            printf("%d ", sub[i][j]);
+
+        printf("\n");
+    }
+
+    return 0;
+}
